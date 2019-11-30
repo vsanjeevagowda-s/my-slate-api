d09fe295bf9ac6ed9ee0d1ef577b7bc4d9fb9ef5
venkatesh <vsanjeevagowda@qwini.io>
2019-11-30 04:55:11 +0000

    list

--- routes/workspace.js
+++ routes/workspace.js
@@ -2,6 +2,7 @@ const workspace = require('../src/api/controllers/workspace');
 
 const workspaceRoutes = (app) => {
   app.post('/api/workspace', workspace.create);
+  app.get('/api/list', workspace.list);
 };
 
 module.exports = workspaceRoutes;
--- src/api/controllers/workspace.js
+++ src/api/controllers/workspace.js
@@ -4,12 +4,22 @@ const create = async (req, res) => {
   try {
     const { date, record } = req.body;
     const workspaceResp = await workspace.create({ date, record });
-    return res.status(200).json({ resp: workspaceResp });
+    return res.status(200).json({ workspace: workspaceResp });
   } catch (error) {
     return res.status(422).json({ error });
   }
 };
 
+const list  = async (req, res) => {
+  try{
+    const workspaceResp = await workspace.find();
+    return res.status(200).json({ workspaces: workspaceResp });
+  }catch(error){
+    return res.status(422).json({ error });
+  }
+}
+
 module.exports = {
   create,
+  list,
 }
