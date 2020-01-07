dd5c7ebb47f705ea707a978b733f6a0f9febce7c
venkatesh <vsanjeevagowda@qwini.io>
2020-01-07 04:53:02 +0000

    socket implementation

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const config = require('./config/environment');
 const dbConnect = require('./models/connect');
+const { Socket } = require('./lib/socket');
 const {
   workspace,
   todo,
@@ -25,9 +28,19 @@ todo(app);
 helper(app);
 session(app);
 
+io.on('connection', (socket) => {
+  console.log('a user is connected')
+  // socket.on('join_room', (room) => {
+  //   socket.join(room);
+  // })
+ const socketClient = new Socket({ client: socket });
+ console.log(`socketClient => ${socketClient}`)
+//  socketClient.registerDisconnectEvent()
+})
+
 dbConnect.on('connected', () => {
   console.log('db connected at: ', config.db_url)
-  const server = app.listen(config.port, () => {
+  http.listen(config.port, () => {
     console.log(`my-slate api listening on port ${config.port}!`);
   });
 });
