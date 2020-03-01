54eed4e5a540703c13dc05650fa99eb6e0cae811
venku <venku@ubuntu-18-workspace-1.asia-south1-a.c.lofty-stack-259514.internal>
2020-03-01 12:09:11 +0000

    version api added

--- routes/workspace.js
+++ routes/workspace.js
@@ -5,6 +5,7 @@ const workspaceRoutes = (app) => {
   app.put('/api/workspace', authUser, workspace.create);
   app.get('/api/workspaces', authUser,  workspace.list);
   app.get('/api/workspace/:date', authUser, workspace.show);
+  app.get('/api/workspace/:date/versions', authUser, workspace.versions);
 };
 
 module.exports = workspaceRoutes;
--- src/api/controllers/workspace.js
+++ src/api/controllers/workspace.js
@@ -24,9 +24,46 @@ const show = async (req, res) => {
     const { date } = req.params;
     const workspaceResp = await Workspace.findOne({ date });
     if (!workspaceResp) throw new Error('Record not found!!')
-    const [workspace, ...items] = workspaceResp.versions.sort((m1, m2) => ( m1.version > m2.version ? -1 : 1 ));
+    const [workspace, ...items] = workspaceResp.versions.sort((m1, m2) => (m1.version > m2.version ? -1 : 1));
     if (!workspace) throw new Error('Verison not available!!')
     return res.status(200).json({ workspace: { date: moment(workspace.date).format('YYYY-MM-DD'), record: workspace.record, version: workspace.version } });
+  } catch (error) {
+    return res.status(422).json({ message: hmve(Workspace, error).message });
+  }
+}
+
+const versions = async (req, res) => {
+  try {
+    const { date } = req.params;
+    let { page = 1, limit=10 } = req.query;
+    page = Number(page);
+    limit = Number(limit);
+    let [workspace, ...rest] = await Workspace.aggregate([
+      {
+        $match: {
+          date: new Date(date)
+        }
+      },
+      {
+        $project: {
+          versions: {
+            $slice: ["$versions", (page - 1) * limit, limit]
+          },
+          totalRecords: {
+            $size: "$versions"
+          }
+        }
+      }
+    ]);
+    if(!workspace.versions.length) throw new Error('No more records available!!');
+    const totalPages = Math.floor(workspace.totalRecords / limit) + 1;
+    return res.status(200).json({
+      ...workspace,
+      totalPages,
+      limit,
+      page,
+      recordsPerPage: workspace.versions.length
+    });
   } catch (error) {
     return res.status(422).json({ message: hmve(Workspace, error).message });
   }
@@ -44,5 +81,6 @@ const list = async (req, res) => {
 module.exports = {
   create,
   list,
-  show
+  show,
+  versions
 }
